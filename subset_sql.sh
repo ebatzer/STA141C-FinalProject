343545a4e5af46bd30389609dc43990074600ecd
Evan Batzer <ebatzer@ucdavis.edu>
2019-03-09 17:12:00 -0800

    Fix sql file path

--- subset_sql.sh
+++ subset_sql.sh
@@ -6,15 +6,19 @@
 # Give the job a name
 #SBATCH --job-name subset_sqlite
 
+# Loading in bio module for sqlite3
 module load bio
 
 printf "SELECT action_date, generate_pragmatic_obligation, total_obligation, awarding_agency_id,
+# Generating SQL command to select relevant rows, filter data
 funding_agency_id, funding_subtier_agency_name
 FROM agency
 WHERE funding_subtier_agency_name = 'U.S. Customs and Border Protection'
 OR funding_subtier_agency_name = 'U.S. Immigration and Customs Enforcement';\n" > select_agencies.sql
 
+# Download file and unzip
 wget http://anson.ucdavis.edu/~clarkf/sta141c/usaspending.sqlite.zip |
   unzip -p > usaspending.sqlite
 
-cat select_agencies.sql | sqlite3 -header -csv ~/data/usaspending.sqlite > selected_agencies.csv
+# Run SQL command and produce subsetted dataset
+cat select_agencies.sql | sqlite3 -header -csv usaspending.sqlite > selected_agencies.csv
