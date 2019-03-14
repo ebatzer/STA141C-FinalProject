bb74ef7a21824bc2972a2315da12eda641bc7338
ebatzer <ebatzer@ucdavis.edu>
2019-03-14 14:39:30 -0700

    Check funding vs. awarding

--- Code/subset_sql.sh
+++ Code/subset_sql.sh
@@ -31,10 +31,25 @@ LEFT JOIN (SELECT DISTINCT name, subtier_agency_id FROM subtier_agency)
 ON awarding_subtier_agency_name=name
 WHERE subtier_agency_id = 778
 OR subtier_agency_id = 776
-OR subtier_agency_id = 257" > filter_rows.sql
+OR subtier_agency_id = 257" > filter_rows_awarding.sql
+
+# Join on meaningful columns
+printf "SELECT award_id, action_date, fiscal_year, total_obligation,
+generated_pragmatic_obligation, transaction_description, award_category,
+pop_country_name, pop_state_code, pop_county_name, pop_zip5, naics_code,
+awarding_subtier_agency_name, funding_subtier_agency_name, business_categories
+FROM universal_transaction_matview
+LEFT JOIN (SELECT DISTINCT name, subtier_agency_id FROM subtier_agency)
+ON funding_subtier_agency_name=name
+WHERE subtier_agency_id = 778
+OR subtier_agency_id = 776
+OR subtier_agency_id = 257" > filter_rows_funding.sql
 
 # Find rows that match our subtier agency criteria
 cat unique_subtiers.sql | sqlite3 -header -csv ${DATAFILE} > subtier_names.csv
 
 # Run SQL command and produce subsetted dataset
-cat filter_rows.sql | sqlite3 -header -csv ${DATAFILE} > filtered_data.csv
+cat filter_rows_awarding.sql | sqlite3 -header -csv ${DATAFILE} > filtered_data_awarding.csv
+
+# Run SQL command and produce subsetted dataset
+cat filter_rows_funding.sql | sqlite3 -header -csv ${DATAFILE} > filtered_data_funding.csv
